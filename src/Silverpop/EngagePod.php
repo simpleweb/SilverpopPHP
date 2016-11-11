6b84cfe086f1ac9d90e1d556a326f89e7802730e
Vit Hovezak <kybersoft@gmail.com>
2016-11-11 11:00:55 +0100

    Add VISITOR_KEY element for add/update methods

--- src/Silverpop/EngagePod.php
+++ src/Silverpop/EngagePod.php
@@ -208,7 +208,7 @@ class EngagePod {
      * Add a contact to a list
      * https://kb.silverpop.com/kb/Engage/API/API_XML/XML_API_Developer_Guide/03_Contact_XML_Interfaces/02_Database_Management_Interfaces_-_Contact/01_Add_a_Contact
      */
-    public function addContact($databaseID, $updateIfFound, $columns, $contactListID = false, $sendAutoReply = false, $allowHTML = false, $createdFrom = 1) {
+    public function addContact($databaseID, $updateIfFound, $columns, $contactListID = false, $sendAutoReply = false, $allowHTML = false, $createdFrom = 1, $visitorKey = '') {
         $data["Envelope"] = array(
             "Body" => array(
                 "AddRecipient" => array(
@@ -217,6 +217,7 @@ class EngagePod {
                     "SEND_AUTOREPLY"  => ($sendAutoReply ? 'true' : 'false'),
                     "UPDATE_IF_FOUND" => ($updateIfFound ? 'true' : 'false'),
                     "ALLOW_HTML" => ($allowHTML ? 'true' : 'false'),
+                    "VISITOR_KEY" => $visitorKey,
                     "CONTACT_LISTS" => ($contactListID) ? array("CONTACT_LIST_ID" => $contactListID) : '',
                     "COLUMN" => array(),
                 ),
@@ -329,13 +330,14 @@ class EngagePod {
      * @throws \Exception
      * @return int recipient ID
      */
-    public function updateContact($databaseID, $oldEmail, $columns) {
+    public function updateContact($databaseID, $oldEmail, $columns, $visitorKey = '') {
         $data["Envelope"] = array(
             "Body" => array(
                 "UpdateRecipient" => array(
                     "LIST_ID"         => $databaseID,
                     "OLD_EMAIL"       => $oldEmail,
                     "CREATED_FROM"    => 1,        // 1 = created manually
+                    "VISITOR_KEY"     => $visitorKey,
                     "COLUMN" => array(),
                 ),
             ),
